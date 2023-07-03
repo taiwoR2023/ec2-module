c699b886f3b0e3aba21c5c0ee2fa5dbd150bbfda
taiwoR2023 <taiwo_osundairo@yahoo.com>
2023-07-03 14:27:32 -0500

    terraform

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+    description = "Amazon machine image"
+    type = string
+  default = "ami-04823729c75214919"
+}
+variable "instancetype" {
+    default = "t2.small"
+  
+}
+variable "region_name" {
+    # default = "us-east-1" 
+  
+}
+variable "profile_name" {
+    default = "default"
+  
+}
